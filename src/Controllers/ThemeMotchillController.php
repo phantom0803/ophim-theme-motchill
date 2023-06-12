801f95725740451dd0e62fd8da265c4c1922225f
Hades <opdlnf01@gmail.com>
2023-06-12 12:48:24 +0700

    Fix paginate search

--- src/Controllers/ThemeMotchillController.php
+++ src/Controllers/ThemeMotchillController.php
@@ -51,7 +51,7 @@ class ThememotchillController
                 if (request('filter')['sort'] == 'view') {
                     return $movie->orderBy('view_total', 'desc');
                 }
-            })->paginate();
+            })->paginate(get_theme_option('per_page_limit'));
 
             return view('themes::thememotchill.catalog', [
                 'data' => $data,
